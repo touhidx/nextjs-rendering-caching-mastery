b744edf3e35900277f00d7342627b5ee2556bbd2
touhidx <touhidul0147@gmail.com>
2026-09-19 23:51:01 +0600

    learn about different types of data fetching

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
-const PostPage = () => {
+const PostPromise = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return res.json();
+};
+
+const PostPage = async () => {
+  //   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //   const posts = await res.json();
+  const posts = await PostPromise();
   return (
     <div>
-      <h1>this is postpage</h1>
+      <h1>this is postpage : {posts.length}</h1>
     </div>
   );
 };
